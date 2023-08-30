c5f1560681fe358d289dd5901af210ee27ab3277
SSSerg05 <ssserg05@gmail.com>
2023-08-30 16:39:44 +0300

    add routes, add functions, add error

--- models/contacts.js
+++ models/contacts.js
@@ -1,16 +1,130 @@
 // const fs = require('fs/promises')
 
-const listContacts = async () => {}
+// const listContacts = async () => {}
 
-const getContactById = async (contactId) => {}
+// const getContactById = async (contactId) => {}
 
-const removeContact = async (contactId) => {}
+// const removeContact = async (contactId) => {}
 
-const addContact = async (body) => {}
+// const addContact = async (body) => {}
 
-const updateContact = async (contactId, body) => {}
+// const updateContact = async (contactId, body) => {}
 
-module.exports = {
+// export default {
+//   listContacts,
+//   getContactById,
+//   removeContact,
+//   addContact,
+//   updateContact,
+// }
+
+import { promises as fs } from 'fs';
+import path from 'path';
+import { nanoid } from 'nanoid';
+
+const contactsPath = path.resolve('db', 'contacts.json');
+
+// Read File DataBase
+//=======================
+// @param {string} [fileName]
+// 
+// @return [{...},{...}] || null - array of objects || null
+const readData = async (fileName) => {
+  try {
+    const data = await fs.readFile(fileName);
+    return JSON.parse(data);
+
+  } catch (error) {
+    console.log(`Cannot read file or file not found. ${error}`);
+  }
+
+  return null;
+}
+
+// Update File DataBase
+//=======================
+// @param {string} data
+// 
+// @return undefined
+const updateData = (data) =>
+  fs.writeFile(contactsPath, JSON.stringify(data, null, 2))
+
+
+// List Contacts
+//=======================
+// @return [{...},{...},{...}] || null
+export const listContacts = async () => {
+
+  const data = await readData(contactsPath);
+
+  return data || null;
+}
+
+
+// get Contact By Id
+//=======================
+// @param {object} {id}
+// 
+// @return {...} || null - object || null
+export const getContactById = async ({ id }) => {
+
+  const data = await readData(contactsPath);
+  const result = data.find(item => item.id === id)
+
+  return result || null;
+}
+
+
+// remove field without DataBase file 
+//=======================
+// @param {object} {id}
+// 
+// @return {...} || null - object || null
+export const removeContact = async({ id }) => {
+
+  const removeField = await getContactById({ id });
+
+  if (!removeField) { 
+    return null
+  }
+  
+  const data = await readData(contactsPath);
+  const result = data.filter(item => item.id !== id)
+  await updateData(result);
+
+  return removeField;
+}
+
+
+// Add Contact in DataBase file
+//=======================
+// @param {object} {name, email, phone}
+// 
+// @return {...} || null - object || null
+export const addContact = async ({ name, email, phone }) => {
+
+  const data = await readData(contactsPath);
+  const newField = {id: nanoid(), name, email, phone};
+  const result = [ ...data, newField ];
+
+  await updateData(result);
+
+  return newField;
+}
+
+// update Contact in DataBase file
+//=======================
+// @param {string} contactId
+// @param {object} {body}
+// 
+// @return {...} || null - object || null
+const updateContact = async (contactId, body) => { 
+  
+  return null;
+}
+
+
+export default {
   listContacts,
   getContactById,
   removeContact,
--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,61 @@
-const express = require('express')
+import express from 'express';
+import db from "../../models/contacts.json";
+import contacts from '../../models/contacts.js';
+
 
 const router = express.Router()
 
+// список всіх контактів
 router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const result = await contacts.listContacts()
+    res.status(200).json(result);
+  } catch (error) {
+    res.status(500).json({message: "Server not found"})
+  }
+
+
 })
 
+// пошук по id
 router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
+  try {
+    const { id } = req.params;
+    const result = await getContactById(id);
+    
+    if (!result) {
+      const error = new Error("Not found")
+      error.status = 404;
+      throw error;
+    }
+    
+    res.status(200).json(result);
+
+  } catch (error) {
+    next(error);
+    // res.status(404).json({ message: 'Not found' })
+  }
+  
 })
 
+// додавання запису
 router.post('/', async (req, res, next) => {
+  try {
+    
+  } catch (error) {
+    
+  }
   res.json({ message: 'template message' })
 })
 
+// видалення запису
 router.delete('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
 })
 
+// 
 router.put('/:contactId', async (req, res, next) => {
   res.json({ message: 'template message' })
 })
 
-module.exports = router
+export default router
