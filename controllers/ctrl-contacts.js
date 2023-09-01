0c988285334c06b7439edddda43dcebafc94dfdf
SSSerg05 <ssserg05@gmail.com>
2023-09-01 15:44:17 +0300

    add decorator

--- controllers/ctrl-contacts.js
+++ controllers/ctrl-contacts.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import Joi from 'joi';
 
-import contacts from '../../models/contacts.js';
-import HttpError from '../../helpers/HttpError.js';
+import contacts from '../models/contacts.js';
+import HttpError from '../helpers/HttpError.js';
+import ctrlWrapper from '../decrator/ctrlWrapper.js';
 
 
 // схема для валідації
@@ -14,107 +15,74 @@ const addSchema = Joi.object({
 
 // список всіх контактів
 const listContacts = async (req, res, next) => {
-  try {
-    const result = await contacts.listContacts();
-    
-    if (!result) {
-      throw HttpError(500, "Server not found");
-    }
-
-    res.json(result);
-  } catch (error) {
-      next(error);
-      //res.status(500).json({message: "Server not found"})
+  const result = await contacts.listContacts();
+  
+  if (!result) {
+    throw HttpError(500, "Server not found");
   }
+  res.json(result);
 }
 
 
 // пошук по id
 const getContactById = async (req, res, next) => {
-  try {
-
-    const { id } = req.params;
-    const result = await contacts.getContactById(id);
-    if (!result) {
-      throw new HttpError(404, "Not found");
-    }
-    
-    res.json(result);
-
-  } catch (error) {
-    next(error);
+  const { id } = req.params;
+  const result = await contacts.getContactById(id);
+  if (!result) {
+    throw new HttpError(404, "Not found");
   }
+  
+  res.json(result);
 }
 
 
 // додавання запису
 const addContact = async (req, res, next) => {
-  try {
-
-    // потрібна перевірка req.body
-    const { error } = addSchema.validate(req.body);
-    if (error) {
-      throw HttpError(400, "Не має даних для додавання. " + error.message);
-    }
-
-    const result = await contacts.addContact(req.body);
-    
-    if (!result) {
-      throw HttpError(404, "Cannot add Contact");
-    }
-    
-    res.status(201).json(result);
-
-  } catch (error) {
-    next(error);
+  const { error } = addSchema.validate(req.body);
+  if (error) {
+    throw HttpError(400, "Не має даних для додавання. " + error.message);
   }
+  const result = await contacts.addContact(req.body);
+  
+  if (!result) {
+    throw HttpError(404, "Cannot add Contact");
+  }
+  
+  res.status(201).json(result);
 }
 
 
 // видалення запису
 const removeContact = async (req, res, next) => {
-  try {
-    const { id } = req.params;
-    const result = await contacts.removeContact(id);
-    
-    if (!result) {
-      throw HttpError(404, `Not found id:${id}`);
-    }
-    
-    res.status(200).json({ ...result, message: "Contact deleted" });
-  } catch (error) {
-      next(error);
+  const { id } = req.params;
+  const result = await contacts.removeContact(id);
+  
+  if (!result) {
+    throw HttpError(404, `Not found id:${id}`);
   }
-
+  
+  res.status(200).json({ ...result, message: "Contact deleted" });
 }
 
 
 // оновлення запису
 const updateContact = async (req, res, next) => {
-  try {
-    const { error } = addSchema.validate(req.body);
-    if (error) { 
-      throw HttpError(400, "Missing fields " + error.message);
-    }
-
-    const { id } = req.params;
-    const result = await contacts.updateContact(id, req.body);
-
-    if (!result) {
-      throw HttpError(404, `Not found contact with id:${id}`);
-    }
-
-    res.json(result);
-
-  } catch (error) {
-    next(error);
+  const { error } = addSchema.validate(req.body);
+  if (error) { 
+    throw HttpError(400, "Missing fields " + error.message);
   }
+  const { id } = req.params;
+  const result = await contacts.updateContact(id, req.body);
+  if (!result) {
+    throw HttpError(404, `Not found contact with id:${id}`);
+  }
+  res.json(result);
 }
 
 export default {
-  listContacts,
-  getContactById,
-  removeContact,
-  addContact,
-  updateContact,
+  listContacts: ctrlWrapper(listContacts),
+  getContactById: ctrlWrapper(getContactById),
+  removeContact: ctrlWrapper(removeContact),
+  addContact: ctrlWrapper(addContact),
+  updateContact: ctrlWrapper(updateContact),
 }
