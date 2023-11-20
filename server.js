1e08b312f62ab20e5440ec282d53be0eaab37dff
SSSerg05 <ssserg05@gmail.com>
2023-11-20 21:31:57 +0200

    add mongoose in server
    Signed-off-by: SSSerg05 <ssserg05@gmail.com>

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 import app from './app.js'
+import mongoose from 'mongoose'
+import HttpError from './helpers/HttpError.js'
 
 // SSerg
 // lUXIBymHzzGQJUWG
 //mongodb+srv://SSerg:<password>@cluster0.w5v2ol5.mongodb.net/
 
+const DB_HOST = "mongodb+srv://SSerg:lUXIBymHzzGQJUWG@cluster0.w5v2ol5.mongodb.net/"
 
+mongoose.connect(DB_HOST)
+  .then( () => console.log("Success connect to database"))
+  .catch(error => console.log(error.message))
 
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000")
