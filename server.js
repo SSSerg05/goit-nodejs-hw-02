0a1272aaeaa68d7740e5b6b10e47853c23b304e3
SSSerg05 <ssserg05@gmail.com>
2023-11-20 16:55:34 +0200

    add path

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ import app from './app.js'
 
 // SSerg
 // lUXIBymHzzGQJUWG
+//mongodb+srv://SSerg:<password>@cluster0.w5v2ol5.mongodb.net/
+
+
 
 app.listen(3000, () => {
   console.log("Server running. Use our API on port: 3000")
