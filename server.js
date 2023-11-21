1951061f6885f7e999b41ac21c5ee6ed0fd8f7c6
SSSerg05 <ssserg05@gmail.com>
2023-11-21 16:08:14 +0200

    close process

--- server.js
+++ server.js
@@ -4,9 +4,14 @@ import mongoose from 'mongoose'
 const DB_HOST = "mongodb+srv://SSerg:lUXIBymHzzGQJUWG@cluster0.w5v2ol5.mongodb.net/db-contacts?retryWrites=true&w=majority"
 
 mongoose.connect(DB_HOST)
-  .then( () => console.log("Success connect to database"))
-  .catch(error => console.log(error.message))
+  .then( () => {
+    console.log("Success connect to database");
+    app.listen(3000, () => {
+      console.log("Server running. Use our API on port: 3000")
+    });
+  })
+  .catch(error => {
+    console.log(error.message)
+    process.exit(1);
+  })
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
-})
