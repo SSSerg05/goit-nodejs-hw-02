749580df4c015f24b0603ff4373ff6bc7d606096
SSSerg05 <ssserg05@gmail.com>
2023-11-28 16:59:40 +0200

    fix

--- models/hooks.js
+++ models/hooks.js
@@ -1,14 +1,11 @@
 // hooks mongoose
-export const handleSaveError = (error, data, next) => {
+export const handleSaveError = (error, data, next)=> {
   error.status = 400;
-  console.log(data, error);
-  console.log(this.options);
-
   next();
-};
+}
 
-export const preUpdate = (error, data, next) => {
+export const preUpdate = function(next) {
   this.options.new = true;
   this.options.runValidators = true;
   next();
-};
+}
