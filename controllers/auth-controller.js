d605a3ab6f9580f8fca8cbae6a89248a5872e639
SSSerg05 <ssserg05@gmail.com>
2023-11-29 21:49:19 +0200

    add signIn
    Signed-off-by: SSSerg05 <ssserg05@gmail.com>

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -6,7 +6,7 @@ import HttpError from '../helpers/HttpError.js';
 import { ctrlWrapper } from '../decorators/index.js';
 
 
-const signUp = async (req, res ) => {
+const signUp = async (req, res) => {
 
   const { email, password } = req.body;
   const user = await User.findOne({email});
@@ -23,8 +23,26 @@ const signUp = async (req, res ) => {
   })
 }
 
+const signIn = async (req, res) => {
+  const {email, password} = req.body;
+  const user = await User.findOne({email});
+  if (!user) {
+    throw HttpError(401, "Email or password invalide");
+  }
+  
+  const passwordCompare = await bcrypt.compare(password, user.password);
+  if (!passwordCompare) {
+    throw HttpError(401, "Email or password invalide");
+  }
+
+  const token = "1111111";
+  res.json({
+    token,
+  })
+
+}
 
 export default {
   signUp: ctrlWrapper(signUp),
-
+  signIn: ctrlWrapper(signIn),
 }
--- routes/api/auth-route.js
+++ routes/api/auth-route.js
@@ -2,12 +2,13 @@ import express from 'express';
 
 import authController from "../../controllers/auth-controller.js";
 import { validateBody } from '../../decorators/index.js'; 
-import { isValidId, isEmptyBody } from '../../middlewares/index.js';
-import { contactAddSchema, contactFavoriteSchema, contactUpdateSchema } from '../../models/Contact.js';
-import { userSignUpSchema } from "../../models/User.js";
+import { isEmptyBody } from '../../middlewares/index.js';
+import { userSignUpSchema, userSignInSchema } from "../../models/User.js";
 
 const authRoute = express.Router();
 
 authRoute.post("/signup", isEmptyBody, validateBody(userSignUpSchema), authController.signUp);
+
+authRoute.post("/signin", isEmptyBody, validateBody(userSignInSchema), authController.signIn);
 
 export default authRoute;
