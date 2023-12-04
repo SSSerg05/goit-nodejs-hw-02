0f4ec9f8c6a0e8b7a48ec14045bed272072fa24c
SSSerg05 <ssserg05@gmail.com>
2023-12-04 16:07:18 +0200

    add middlevare upload

--- middlewares/upload.js
+++ middlewares/upload.js
@@ -1,4 +1,5 @@
 import multer from "multer";
+import HttpError from "../helpers/HttpError.js";
 
 const destination = path.resolve("tmp");
 
@@ -17,3 +18,19 @@ const limits = {
   fileSize: 5 * 1024 * 1024, //(5MB)
 }
 
+const fileFilter = (req, file, cb) => {
+  const extention = file.originalname.split('.').pop();
+  if (extention === 'exe') {
+    cb(HttpError(400, "Invalid file extention"));
+  }
+
+  cb(null, true);
+}
+
+const upload = multer({
+  storage,
+  limits,
+  fileFilter,
+})
+
+export default upload;
