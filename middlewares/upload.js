0c9128c90e5e45747873626013651cdd85b191de
SSSerg05 <ssserg05@gmail.com>
2023-12-04 13:57:02 +0200

    HW05 start

---
+++ middlewares/upload.js
@@ -0,0 +1,19 @@
+import multer from "multer";
+
+const destination = path.resolve("tmp");
+
+// storage, limits, fileFilter
+const storage = multer.diskStorage({
+  destination,
+  filename: (req, res,cb) => {
+    const uniquePrefix = `${Date.now()}_${Math.round(Math.random() * 1E9)}`
+    const filename = `${uniquePrefix}_${file.originalname}`
+    
+    cb(null, file.originalname);
+  },
+})
+
+const limits = {
+  fileSize: 5 * 1024 * 1024, //(5MB)
+}
+
