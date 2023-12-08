289ee34fa8691e99c6487807405929d5ad94ebf2
SSSerg05 <ssserg05@gmail.com>
2023-12-08 14:17:07 +0200

    fix sendEmail

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -3,26 +3,33 @@ import "dotenv/config";
 
 const {UKR_NET_PASSWORD, UKR_NET_EMAIL} = process.env;
 
-const nodemailerConfig = {
-  host: "smtp.ukr.net",
-  port: 465,
-  secure: true,
-  auth: {
-    user: UKR_NET_EMAIL,
-    pass: UKR_NET_PASSWORD,
-  }
-}
-
-const transport = nodemailer.createTransport(nodemailerConfig);
-
-const email = {
-  from: UKR_NET_EMAIL,
+/* data = 
   to: "users@email.com",
   subject: "Test email",
   html: "<strong>Test Email</strong>",
+*/
+const sendEmail = async (data) => {
+  const nodemailerConfig = {
+    host: "smtp.ukr.net",
+    port: 465,
+    secure: true,
+    auth: {
+      user: UKR_NET_EMAIL,
+      pass: UKR_NET_PASSWORD,
+    }
+  }
+
+  const transport = nodemailer.createTransport(nodemailerConfig);
+
+  const email = {...data, from: UKR_NET_EMAIL};
+  try {
+    await transport.sendMail(email);
+    return console.log("Email send sucess");
+  } catch (error) {
+    return console.log(error.message);
+  }
 }
 
-transport.sendMail(email)
-  .then(() => console.log("Email send sucuss"))
-  .catch(error => console.log(error.message));
 
+
+  export default sendEmail;
