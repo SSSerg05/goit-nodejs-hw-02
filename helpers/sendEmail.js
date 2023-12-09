fbe46a1f39c2478c5d84187c88214d6a697b7af2
SSSerg05 <ssserg05@gmail.com>
2023-12-09 13:55:36 +0200

    fix
    Signed-off-by: SSSerg05 <ssserg05@gmail.com>

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -14,18 +14,13 @@ const nodemailerConfig = {
 
 const transport = nodemailer.createTransport(nodemailerConfig);
 
-/* data = 
-  to: "users@email.com",
-  subject: "Test email",
-  html: "<strong>Test Email</strong>",
-*/
 const sendEmail = async (data) => {
   const email = {...data, from: UKR_NET_EMAIL};
+  console.log(email);
+
   await transport.sendMail(email)
-    .then(() => console.log("Email send sucess"))
-    .catch(error => console.log(error.message));
-  
-  return console.log(`Email to=${data.to} send sucess`);
+    .then(() => console.log(`Email to=${data.to} send sucess`))
+    .catch(error => console.log(`Wrong send email. ${error.message}`));
 }
 
 export default sendEmail;
